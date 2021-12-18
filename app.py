3b43522ec5f19d2ab725e8941f50dd5fd6513481
Shafira Naya <shfrnaya@gmail.com>
2021-12-18 07:56:33 +0700

    add index route

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from main import *
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
 @app.route('/recommendation', methods=['GET','POST'])
 def recommendation():
     product_name_input = request.form.get("inputProductName")
